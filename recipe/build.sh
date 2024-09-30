45dd0995cadb46a77bc94daa19c4062960dae8c8
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2024-09-30 14:58:46 -0400

    Update to version 1.0.17537.20

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-mkdir build
-cd build
+set -ex
 
 export CXXFLAGS="$CXXFLAGS -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS"
 
 cmake ${CMAKE_ARGS} \
-    -DLLVM_DIR=$PREFIX/lib/cmake/llvm \
+    -DCMAKE_VERBOSE_MAKEFILE=TRUE \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DIGC_TARGET__TOOLS_CLANG_DIR=$PREFIX/lib \
-    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_DIR=$PREFIX/lib/cmake/llvm \
     -DCOMMON_CLANG_LIBRARY_NAME=opencl_clang \
     -DINTRSRC=$SRC_DIR/vc-intrinsics/GenXIntrinsics \
-    -DIGC_OPTION__USE_KHRONOS_SPIRV_TRANSLATOR=1 \
-    -DSPIRV_TRANSLATOR_DIR=$PREFIX \
-    -DIGC_OPTION__LLVM_FROM_SYSTEM=1 \
-    -DINSTALL_SPIRVDLL=0 \
-    -DIGC_OPTION__LLVM_PREFERRED_VERSION=10.0.0 \
-    -DFOUND_VCS=$PREFIX/lib/cmake/llvm/VersionFromVCS.cmake \
-    ..
+    -DIGC_OPTION__LLVM_MODE=Prebuilds \
+    -DIGC_OPTION__LLVM_PREFERRED_VERSION=${LLVM_VERSION}.0\
+    -DIGC_OPTION__SPIRV_TOOLS_MODE=Prebuilds \
+    -DIGC_OPTION__USE_PREINSTALLED_SPIRV_HEADERS=ON \
+    -S ./igc \
+    -B ./build
 
-make -j${CPU_COUNT}
-make install
-
+cmake --build ./build
+cmake --build ./build --target install
